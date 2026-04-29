144e6e02dce378cb741fe3bad28a07b78f935870
mohamed yasser <mohamed.tech.yasser.2006@gmail.com>
2026-04-29 16:15:54 +0300

    Update ISubject.cs
    who want to send notification should have...

--- Budget app/Interaces/ISubject.cs
+++ Budget app/Interaces/ISubject.cs
@@ -1,1 +1,9 @@
-﻿
+﻿namespace BudgetApp.Interfaces
+{
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify(string message);
+    }
+}
