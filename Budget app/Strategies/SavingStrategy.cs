fbc14f007fcd5cdbe235fc352d0c47943df72b07
mohamed yasser <mohamed.tech.yasser.2006@gmail.com>
2026-04-29 16:38:04 +0300

    Update SavingStrategy.cs
    save20%

--- Budget app/Strategies/SavingStrategy.cs
+++ Budget app/Strategies/SavingStrategy.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using BudgetApp.Interfaces;
+
+namespace BudgetApp.Strategies.Interfaces
+{
+    public class SavingStrategy : ICalculationStrategy
+    {
+        public string StragyName => "Saving";
+
+        public double Calculate(double Balance, int daysLeft)
+        {
+            return (Balance / daysLeft) * 0.20;
+        }
+    }
+}
