497c9f0563ab8d2f069619e378ae741972b0168d
mohamed yasser <mohamed.tech.yasser.2006@gmail.com>
2026-04-29 16:07:55 +0300

    Update ICalculationStrategy.cs

--- Budget app/Interaces/ICalculationStrategy.cs
+++ Budget app/Interaces/ICalculationStrategy.cs
@@ -1,1 +1,10 @@
+namespace BudgetApp.Interfaces
+
+{
+    public interface ICalculationStrategy
+    {
+        string StragyName { get; }
+        double Calculate(double balance, int days);
+    }
+}
 ﻿
