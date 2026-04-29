ea1ec291a13f10bf1034fe9d15f0ab1f3e4ee8f8
mohamed yasser <mohamed.tech.yasser.2006@gmail.com>
2026-04-29 16:12:19 +0300

    Update IDatabase.cs

--- Budget app/Interaces/IDatabase.cs
+++ Budget app/Interaces/IDatabase.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using BudgetApp.Interfaces;
+namespace BudgetApp.Interfaces
+{
+    public interface IDatabase
+    {
+        void Insert(object entity);
+        void Update(object entity);
+        void Delete(object entity);
+        List<T> FetchAll<T>() where T : class;
+    }
+}
