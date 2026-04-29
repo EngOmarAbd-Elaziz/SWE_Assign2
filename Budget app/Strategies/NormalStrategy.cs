d8550ae620a2443f1bf25bff381de182afd295f4
mohamed yasser <mohamed.tech.yasser.2006@gmail.com>
2026-04-29 16:31:30 +0300

    Update NormalStrategy.cs
    money/days

--- Budget app/Strategies/NormalStrategy.cs
+++ Budget app/Strategies/NormalStrategy.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using BudgetApp.Interfaces;
+
+namespace BudgetApp.Strategies.Interfaces
+
+{
+
+    public class NormalStrategy : ICalculationStrategy
+    {
+        public string StragyName => "Normal";
+
+        public double Calculate(double Balance, int daysLeft)
+        {
+            return Balance / daysLeft;
+        }
+    }
+}
