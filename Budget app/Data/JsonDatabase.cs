fd8f1446f040b09acd80ebf334ee6dc077317b1a
mohamed yasser <mohamed.tech.yasser.2006@gmail.com>
2026-04-30 12:05:03 +0300

    Update JsonDatabase.cs
    the form of data in json

--- Budget app/Data/JsonDatabase.cs
+++ Budget app/Data/JsonDatabase.cs
@@ -1,12 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using BudgetApp.Interfaces;
+using BudgetApp.Models;
 
-namespace Budget_app.Data
+namespace BudgetApp.Data
 {
     internal class JsonDatabase
     {
+        public List<BudgetCycle> BudgetCycles { get; set; } = new List<BudgetCycle>();
+        public List<Expense> Expenses { get; set; } = new List<Expense>();
+        public List<Category> Categories { get; set; } = new List<Category>();
+        public int NextCycleId { get; set; } = 1;
+        public int NextExpenseId { get; set; } = 1;
+        public int NextCategoryId { get; set; } = 1;
+
     }
+
 }
